62beeaea3478bb4349844dc85df339bfc5c8a825
babajigits <125106530+babajigits@users.noreply.github.com>
2023-02-11 08:40:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('Badge 1 has been completed')
 streamlit.text('It was fun learning')
 streamlit.text('I will complete the badge 2 also')
 streamlit.text('I will update on linkedin after the badge 2')
+
+import pandas
+my_fruit_list=pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

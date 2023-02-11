af024229e31485f2f508dbc6c770371b6314f32a
babajigits <125106530+babajigits@users.noreply.github.com>
2023-02-11 08:44:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,11 @@ streamlit.text('I will update on linkedin after the badge 2')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# we will add a multiselect so that users can pick what they want
+streamlit.multiselect("Pick some Fruits:", list(my _fruit_list.index))
+
+#Display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+

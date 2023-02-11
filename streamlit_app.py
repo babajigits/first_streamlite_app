a40ee279b77ec841f2a7268eed9ae59acf4d1a38
babajigits <125106530+babajigits@users.noreply.github.com>
2023-02-11 08:36:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Complete Badges in Snowflake')
+streamlit.header('Badge 1 has been completed')
+streamlit.text('It was fun learning')
+streamlit.text('I will complete the badge 2 also')
+streamlit.text('I will update on linkedin after the badge 2')

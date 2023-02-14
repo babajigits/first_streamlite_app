034bfa8ab2cfe51548557c66842f0453cc593b00
babajigits <125106530+babajigits@users.noreply.github.com>
2023-02-14 19:19:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
-#snowflake connector 
-import snowflake.connector
+
 
 import streamlit
 streamlit.title('Complete Badges in Snowflake')
@@ -42,6 +41,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - creating a dataframe from normalized data 
 streamlit.dataframe(fruityvice_normalized)
+
+#snowflake connector 
+import snowflake.connector
 
 
 

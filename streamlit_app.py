59a46e442fb86f7e6ac0f10dbdcc342faefc4a8a
babajigits <125106530+babajigits@users.noreply.github.com>
2023-02-14 20:38:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,14 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 
 #Fruitvice API response
+#create a new code block to get repeated entry 
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+
 #New Section to display fruityvice api response
 streamlit.header( 'Fruitvvice Fruit Advice! ')
 try:
@@ -38,9 +46,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruitvvice_normalized = pandas.json_normalize(fruitvvice_response.json())
-    streamlit.dataframe(fruitvvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit. error()
 
